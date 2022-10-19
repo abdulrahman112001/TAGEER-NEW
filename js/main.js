337424091caf531f37ee52bdb3a3c3ff6ebef665
abdulrahman112001 <101294492+abdulrahman112001@users.noreply.github.com>
2022-10-20 00:19:13 +0200

    p

--- js/main.js
+++ js/main.js
@@ -102,3 +102,33 @@ new PerformanceObserver((entryList) => {
   }
 }).observe({type: 'layout-shift', buffered: true});
 
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      // loop:true,
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 4,
+        },
+      },
+
+    });
+  
